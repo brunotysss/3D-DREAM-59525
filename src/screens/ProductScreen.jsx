ac586eebfd67d539adcc81a21b7a88b467d7d364
brunotysss <brunogymnas@gmail.com>
2024-10-23 13:03:35 -0300

    agregando estrellas a los productos

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,9 +1,14 @@
 import { StyleSheet, Image, View ,FlatList ,Text} from 'react-native'
 import products from '../data/products.json'
 import FlatCard from '../components/FlatCard'
+import StarsRating  from '../components/StarsRating0'
+import useProductRating from '../hooks/useProductRating'; // Importamos el hook para calcular el rating
+
+
 
 const ProductScreen = (item) => {
         const renderProductItem = ({item})=>{
+            const [promedioRating, totalResenas] = useProductRating(item.id); // Obtenemos el promedio de rating y cantidad de reseñas
             return(
                 <FlatCard style={styles.productContainer}>
                     <View>
@@ -16,11 +21,19 @@ const ProductScreen = (item) => {
                     <View>
                         <Text>{item.title}</Text>
                         <Text>{item.shortDescription}</Text>
+
+                            <View>
+                                <StarsRating rating={promedioRating}/>
+                                <Text>({totalResenas} reseñas)</Text>
+
+                            </View>
+
                         <FlatList
                             data = {item.tags}
                             keyExtractor={()=>Math.random()}
                             renderItem={({item})=><Text>{item}</Text>}
                         />
+
                         {
                             item.discount > 0 && <Text>Descuento : {item.discount}</Text>
                         }
@@ -35,7 +48,6 @@ const ProductScreen = (item) => {
    data={products}
    keyExtractor={item=> item.id}
    renderItem={renderProductItem}
-   
    
    />
   )
@@ -59,6 +71,30 @@ const styles = StyleSheet.create({
     },
     productDescription:{
         with : '75%'
+    },
+    productInfo: {
+        flex: 1,
+    },
+    productTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    price: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#4caf50',
+    },
+    ratingContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 5,
+    },
+    tag: {
+        
+        backgroundColor: '#e0e0e0',
+        borderRadius: 5,
+        paddingHorizontal: 5,
+        marginRight: 5,
     }
 
 
