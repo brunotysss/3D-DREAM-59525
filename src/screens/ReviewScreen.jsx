3fccdbb4404c50ad585e5655612773daa49fe718
brunotysss <brunogymnas@gmail.com>
2024-11-20 12:18:06 -0300

    fix buscador y reviews

---
+++ src/screens/ReviewScreen.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ReviewScreen = () => {
+
+
+
+
+
+  return (
+    <View>
+      <Text>ReviewScreen</Text>
+    </View>
+  )
+}
+
+
+
+
+export default ReviewScreen
+
+const styles = StyleSheet.create({})
